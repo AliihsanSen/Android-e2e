7ebc737640e8caa087124620f658b53f62e23dfa
Ali İhsan Şen <av.aliihsan1@gmail.com>
2023-06-22 13:23:48 +0300

    22.06.2023

--- src/test/java/pages/Homepages.java
+++ src/test/java/pages/Homepages.java
@@ -1,6 +1,15 @@
 package pages;
 
+import io.appium.java_client.pagefactory.AppiumFieldDecorator;
+import org.openqa.selenium.support.PageFactory;
+import utils.Driver;
+
+import java.time.Duration;
+
 public class Homepages {
 
+    public Homepages() {
 
+        PageFactory.initElements(new AppiumFieldDecorator(Driver.getAppiumDriver(), Duration.ofSeconds(25)), this);
+    }
 }
